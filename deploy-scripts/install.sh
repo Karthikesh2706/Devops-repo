e5971de01102b87f12d00f897fbdc3c465327faa
Karthikesh2706 <152067431+Karthikesh2706@users.noreply.github.com>
2025-08-28 23:00:57 +0530

    Update install.sh

--- deploy-scripts/install.sh
+++ deploy-scripts/install.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-
-# Update system packages
-sudo apt update -y
-
-# Install Nginx if not installed
-sudo apt install nginx -y
-
-# Enable and start Nginx service
-sudo systemctl enable nginx
-sudo systemctl start nginx
-
-# Set correct permissions for web root
-sudo chown -R www-data:www-data /var/www/html
-sudo chmod -R 755 /var/www/html
-
-# Remove default Nginx page
-sudo rm -f /var/www/html/index.nginx-debian.html
+sudo rm -f /var/www/html/index.html
+sudo cp /home/ubuntu/index.html /var/www/html/
+sudo systemctl restart nginx
